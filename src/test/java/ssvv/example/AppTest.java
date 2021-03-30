68cc9bf04277c06a812af07aac08bc95f9101800
Pascanu Stefan <virgil_stefan2000@yahoo.com>
2021-03-30 12:09:28 +0300

    added test cases

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -3,19 +3,15 @@ package ssvv.example;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import ssvv.example.domain.Student;
-import ssvv.example.repository.NotaXMLRepo;
-import ssvv.example.repository.StudentXMLRepo;
-import ssvv.example.repository.TemaXMLRepo;
-import ssvv.example.service.Service;
-import ssvv.example.validation.NotaValidator;
-import ssvv.example.validation.StudentValidator;
-import ssvv.example.validation.TemaValidator;
-public class AppTest
-{
+import ssvv.example.domain.*;
+import ssvv.example.repository.*;
+import ssvv.example.service.*;
+import ssvv.example.validation.*;
+
+public class AppTest {
     Service testService;
 
-    private void initializeData(){
+    private void initializeData() {
         if (testService == null) {
             StudentValidator studentValidator = new StudentValidator();
             TemaValidator temaValidator = new TemaValidator();
@@ -32,32 +28,95 @@ public class AppTest
         }
     }
 
-    private Student getValidStudentInput(){
+    private Student getValidStudentInput() {
         return new Student("idTest1", "Dan", 935, "dan@ubbcluj.ro");
     }
 
-    private Student getInvalidStudentInput_missingEmail(){
-        return new Student("idTest2", "Dan",935,"");
+    private Student getInvalidStudentInput_missingEmail() {
+        return new Student("idTest2", "Dan", 935, "");
+    }
+
+    private Student getInvalidStudentInput_negativeGroup(){
+        return new Student("idTest2", "Dan", -999, "ejfpjep@eif.com");
+    }
+
+    private Student getInvalidStudentInput_invalidName(){
+        return new Student("idTest2", "", 935, "ejfpjep@eif.com");
     }
 
     @Test
-    public void addStudentValidInput_ShouldReturnTrue()
-    {
+    public void addStudentValidInput_ShouldReturnTrue() {
         initializeData();
         Student testStudent = getValidStudentInput();
-        try{
+        try {
             Student addedTestStudent = testService.addStudent(testStudent);
-            assert(addedTestStudent.getID().equals(testStudent.getID()));
-        }catch (Exception ex){
-            assert(false);
+            assert (addedTestStudent.getID().equals(testStudent.getID()));
+        } catch (Exception ex) {
+            assert (false);
         }
     }
 
     @Test
-    public void addStudentInvalidInput_ShouldReturnFalse()
-    {
+    public void addStudentInvalidInput_ShouldReturnFalse() {
         initializeData();
         Student testStudent = getInvalidStudentInput_missingEmail();
+        try {
+            testService.addStudent(testStudent);
+            assert (false);
+        } catch (Exception ex) {
+            assert (true);
+        }
+    }
+
+    @Test
+    public void addStudentInvalidEmail_ShouldReturnFalse() {
+        initializeData();
+        Student testStudent = getInvalidStudentInput_negativeGroup();
+        try {
+            testService.addStudent(testStudent);
+            assert (false);
+        } catch (Exception ex) {
+            assert (true);
+        }
+    }
+
+    @Test
+    public void addStudentInvalidName_ShouldReturnFalse() {
+        initializeData();
+        Student testStudent = getInvalidStudentInput_invalidName();
+        try {
+            testService.addStudent(testStudent);
+            assert (false);
+        } catch (Exception ex) {
+            assert (true);
+        }
+    }
+
+    private Student getInvalidStudentInput_emptyId(){
+        return new Student("", "Dan", 935, "dan@ubbcluj.ro");
+    }
+
+    private Student getInvalidStudentInput_nullId(){
+        return new Student(null, "Dan", 935, "dan@ubbcluj.ro");
+    }
+    @Test
+    public void addStudentInvalidInput_EmptyId_ShouldReturnFalse()
+    {
+        initializeData();
+        Student testStudent = getInvalidStudentInput_emptyId();
+        try{
+            testService.addStudent(testStudent);
+            assert(false);
+        }catch (Exception ex){
+            assert(true);
+        }
+    }
+
+    @Test
+    public void addStudentInvalidInput_NullId_ShouldReturnFalse()
+    {
+        initializeData();
+        Student testStudent = getInvalidStudentInput_nullId();
         try{
             testService.addStudent(testStudent);
             assert(false);
